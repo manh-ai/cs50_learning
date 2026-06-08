ed9b3fb18467a4931ba1ded520dc84fbfc40c9d8
manh-ai <sonofthegod07@gmail.com>
2026-06-08 13:27:15 +0700

    Add files via upload

---
+++ poiter_cs50.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main(void){
+    int *x ;
+    int *y ;
+    int *x = malloc(sizeof(int));
+    int *x = 18;
+    int *y = 19;
+    
+}
